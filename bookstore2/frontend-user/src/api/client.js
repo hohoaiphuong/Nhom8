c173d3d3de58b66fe8526e1ba2f04f65cb7ed0f2
phuong123 <hop921727@gamil.com>
2026-04-21 13:11:59 +0700

    Cap nhat link API Backend sang Render

--- bookstore2/frontend-user/src/api/client.js
+++ bookstore2/frontend-user/src/api/client.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:8000/api';
+const API_BASE_URL = 'https://nhom8-backend-laravel-1.onrender.com/api';
 
 const client = axios.create({
   baseURL: API_BASE_URL,
